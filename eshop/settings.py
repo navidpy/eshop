bf806d031da8bf9d5f8e1343a3a46a9833cee12e
Navid nematzadeh <76691236+navidpy@users.noreply.github.com>
2021-12-13 13:31:15 +0330

    Update settings.py

--- eshop/settings.py
+++ eshop/settings.py
@@ -145,21 +145,3 @@ MEDIA_ROOT = "static_cdn/media_root"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-GOOGLE_RECAPTCHA_SITE_KEY = "6Ld4SLMaAAAAAMwebQFeW5GZSNiWtVCIOW7Jf8FD"
-GOOGLE_RECAPTCHA_SECRET_KEY = "6Ld4SLMaAAAAABpEbks0bz_Ode4qKXhr5IrWRETf"
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST_USER = 'navidqwertyuiop6@gmail.com'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = 'navid1383'
-
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# # EMAIL_BACKEND = ("django.core.mail.backends.console.EmailBackend")
-# EMAIL_HOST_USER = 'info@navidpy.ir'
-# EMAIL_HOST = 'mail.navidpy.ir'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = False
-# EMAIL_HOST_PASSWORD = 'navid@1383'
-# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
